0b3dc5991bff0138a01dd76509e05d6ba7cf0074
PeeHaa <info@pieterhordijk.com>
2013-05-03 23:41:35 +0200

    Added jump code

---
+++ js/Jump.js
@@ -0,0 +1,27 @@
+function Jump(x, y) {
+    this.info = {
+        x: x,
+        y: y,
+        environment: [],
+        ships: [],
+        isActive: false
+    };
+
+    this.isInRange = function(x, y) {
+        if (x > this.info.x && x < (this.info.x + 15) && y > this.info.y && y < (this.info.y + 15)) {
+            return true;
+        }
+
+        return false;
+    };
+
+    this.isInRangeForPath = function(x, y) {
+        if (x > (this.info.x - Jump.MAXIMUM_DISTANCE_FOR_PATH) && x < (this.info.x + Jump.MAXIMUM_DISTANCE_FOR_PATH) && y > (this.info.y - Jump.MAXIMUM_DISTANCE_FOR_PATH) && y < (this.info.y + Jump.MAXIMUM_DISTANCE_FOR_PATH)) {
+            return true;
+        }
+
+        return false;
+    };
+}
+
+Jump.MAXIMUM_DISTANCE_FOR_PATH = 150;
